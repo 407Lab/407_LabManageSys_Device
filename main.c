f5509f986eace829d000ffcb0a8beccbba213fac
李思洁 <48515272+lisijiell@users.noreply.github.com>
2019-05-25 14:14:31 +0800

    rfid

--- main.c
+++ main.c
@@ -1,62 +1,90 @@
-#include "stm32f4xx.h"
-#include "led.h"
+#include "stm32f10x.h"
+#include "rfid.h"
+#include "lcd.h"
+#include "key.h"
+#include "sys.h"
 #include "delay.h"
-void My_USART1_Init(void)
-{
-	GPIO_InitTypeDef GPIO_InitStructure;
-	USART_InitTypeDef USART_InitStructure;
-	NVIC_InitTypeDef NVIC_InitStruct;
-	
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);//使能USART1时钟
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);//使能GPIO时钟
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource9,GPIO_AF_USART1);//PA9映射到串口1
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource10,GPIO_AF_USART1);//PA10映射到串口1
-	
-	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_9;
-	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_OType=GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd=GPIO_PuPd_UP;
-	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA,&GPIO_InitStructure);//对PA9的初始化
-	
-	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_10;
-	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_AF;//模式为复用
-	GPIO_InitStructure.GPIO_OType=GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd=GPIO_PuPd_UP;
-	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA,&GPIO_InitStructure);
-	
-	USART_InitStructure.USART_BaudRate=115200;
-	USART_InitStructure.USART_HardwareFlowControl=USART_HardwareFlowControl_None;
-	USART_InitStructure.USART_Mode=USART_Mode_Rx|USART_Mode_Tx;
-	USART_InitStructure.USART_Parity=USART_Parity_No;
-	USART_InitStructure.USART_StopBits=USART_StopBits_1;
-	USART_InitStructure.USART_WordLength=USART_WordLength_8b;
-	USART_Init(USART1,&USART_InitStructure);
-	
-	USART_Cmd(USART1,ENABLE);
-	USART_ITConfig(USART1,USART_IT_RXNE,ENABLE);//收到数据后产生中断     使能中断
-	
-	NVIC_InitStruct.NVIC_IRQChannel=USART1_IRQn;
-	NVIC_InitStruct.NVIC_IRQChannelCmd=ENABLE;
-	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority=1;
-	NVIC_InitStruct.NVIC_IRQChannelSubPriority=1;
-	NVIC_Init(&NVIC_InitStruct);
+#include "led.h"
+#include "usart.h"
+/************************************************
+ ALIENTEK 精英STM32F103开发板实验0
+ 工程模板
+ 注意，这是手册中的新建工程章节使用的main文件 
+ 技术支持：www.openedv.com
+ 淘宝店铺：http://eboard.taobao.com 
+ 关注微信公众平台微信号："正点原子"，免费获取STM32资料。
+ 广州市星翼电子科技有限公司  
+ 作者：正点原子 @ALIENTEK
+************************************************/
 
-}
-void USART1_IRQHandler(void)
-{
-	u8 res;
-	if(USART_GetITStatus(USART1,USART_IT_RXNE)){
-	res=USART_ReceiveData(USART1);
-		USART_SendData(USART1,res);
-	}
-}
+
+extern char Card_flag;			//内读卡器检测到卡标志  0 ：无，1：有
+//extern unsigned char SN;
 
 int main(void)
 {
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-	My_USART1_Init();
-	while(1);	
+	delay_init();	    							 //延时函数初始化
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);  //设置NVIC中断分组2:2位抢占优先级，2位响应优先级	
+  LED_Init();                      //初始化LED
+	uart_init(115200);	 	//串口初始化为115200
+  LCD_Init();				//LCD初始化	 
+	KEY_Init();       //KEY初始化
+	InitRC522();
+	POINT_COLOR=RED;
+	LCD_ShowString(30,40,210,24,24,"cccFFFF");
+	delay_ms(1000);
+	
+  while(1)
+	{
+	  if(KEY0==0||KEY1==0)//进入用户录入方式
+	  {
+			LCD_Clear(WHITE);    //清屏
+		  POINT_COLOR=BLUE;
+		  LCD_ShowString(30,70,210,24,24,"ccc欢迎进入录入模式");		
+				
+		  ReadID();  //读卡
+			if(Card_flag==1)	//如果检测到IC卡
+		  {
+		  	POINT_COLOR=BLUE;
+			  LCD_ShowString(30,100,210,24,24,"*****************");			
+				delay_ms(500);
+			  POINT_COLOR=BLUE;
+			  LCD_ShowString(30,130,200,16,16,"111111111111");
+				LCD_ShowString(156,130,200,16,16,(u8 *)num);
+		  	LED1=~LED1;
+        Card_flag=0;      //内刷卡清零				
+				delay_ms(500);
+				
+				
+				ReadID();  //读卡
+				if(Card_flag==1)  //第二次录卡
+		    {				
+			    POINT_COLOR=BLUE;
+		      LCD_ShowString(30,150,180,16,16,"22222222222");
+			    LCD_ShowString(156,150,200,16,16,(u8 *)num);
+          Card_flag=0;      //内刷卡清零     				
+		      delay_ms(500);
+	      }    
+		  }
+		
+//		  if(RFID_ICID==RFID_ID)
+//		  {
+//				LCD_Clear(YELLOW);
+//		  	POINT_COLOR=BLUE;
+//		    LCD_ShowString(30,180,200,16,16,"MMM注册成功");	
+//		  }
+//	    else
+//		  {
+
+//		  	POINT_COLOR=BLUE;
+//			  LCD_ShowString(30,210,200,16,16,"LLL两次刷卡信息不同，请重试");
+//		  }
+	   }
+
+    else delay_ms(10);
+
+	}
+		
 }
+
 
