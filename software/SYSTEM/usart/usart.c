cdf952071dcf58985a4106b9b94044873b397a13
鲁亮 <806034232@qq.com>
2019-07-16 22:20:46 +0800

    Create software projectV1.0

--- software/SYSTEM/usart/usart.c
+++ software/SYSTEM/usart/usart.c
@@ -1,65 +1,49 @@
+/******************************************************************************
+* @ File name --> usart.c
+* @ Author    --> By@ 鲁亮
+* @ Version   --> V1.0
+* @ Date      --> 07 - 16 - 2019
+* @ Brief     --> 系统串口驱动程序
+*
+* @ Copyright (C) 2019
+* @ All rights reserved	www.cqutlab.club
+*******************************************************************************
+*
+*                                  File Update
+* @ Version   --> V1.0
+* @ Author    --> By@鲁亮
+* @ Date      --> 07 - 16 - 2019
+* @ Revise    --> 新建文件
+*
+******************************************************************************/
 #include "sys.h"
 #include "usart.h"	
-////////////////////////////////////////////////////////////////////////////////// 	 
-//如果使用ucos,则包括下面的头文件即可.
-#if SYSTEM_SUPPORT_OS
-#include "includes.h"					//ucos 使用	  
-#endif
-//////////////////////////////////////////////////////////////////////////////////	 
-//本程序只供学习使用，未经作者许可，不得用于其它任何用途
-//ALIENTEK STM32F4探索者开发板
-//串口1初始化		   
-//正点原子@ALIENTEK
-//技术论坛:www.openedv.com
-//修改日期:2014/6/10
-//版本：V1.5
-//版权所有，盗版必究。
-//Copyright(C) 广州市星翼电子科技有限公司 2009-2019
-//All rights reserved
-//********************************************************************************
-//V1.3修改说明 
-//支持适应不同频率下的串口波特率设置.
-//加入了对printf的支持
-//增加了串口接收命令功能.
-//修正了printf第一个字符丢失的bug
-//V1.4修改说明
-//1,修改串口初始化IO的bug
-//2,修改了USART_RX_STA,使得串口最大接收字节数为2的14次方
-//3,增加了USART_REC_LEN,用于定义串口最大允许接收的字节数(不大于2的14次方)
-//4,修改了EN_USART1_RX的使能方式
-//V1.5修改说明
-//1,增加了对UCOSII的支持
-////////////////////////////////////////////////////////////////////////////////// 	  
- 
 
-//////////////////////////////////////////////////////////////////
-//加入以下代码,支持printf函数,而不需要选择use MicroLIB	  
+
 #if 1
-#pragma import(__use_no_semihosting)             
-//标准库需要的支持函数                 
-struct __FILE 
-{ 
-	int handle; 
-}; 
+#pragma import(__use_no_semihosting)
+//标准库需要的支持函数
+struct __FILE
+{
+	int handle;
+};
 
-FILE __stdout;       
+FILE __stdout;
 //定义_sys_exit()以避免使用半主机模式    
-int _sys_exit(int x) 
-{ 
-	x = x; 
-} 
+int _sys_exit(int x)
+{
+	x = x;
+	return x;
+}
 //重定义fputc函数 
 int fputc(int ch, FILE *f)
-{ 	
-	while((USART1->SR&0X40)==0);//循环发送,直到发送完毕   
-	USART1->DR = (u8) ch;      
+{
+	while((USART1->SR&0X40)==0);//循环发送,直到发送完毕
+	USART1->DR = (u8) ch;
 	return ch;
 }
 #endif
- 
-#if EN_USART1_RX   //如果使能了接收
-//串口1中断服务程序
-//注意,读取USARTx->SR能避免莫名其妙的错误   	
+
 u8 USART_RX_BUF[USART_REC_LEN];     //接收缓冲,最大USART_REC_LEN个字节.
 //接收状态
 //bit15，	接收完成标志
@@ -102,8 +86,7 @@ void uart_init(u32 bound){
   USART_Cmd(USART1, ENABLE);  //使能串口1 
 	
 	//USART_ClearFlag(USART1, USART_FLAG_TC);
-	
-#if EN_USART1_RX	
+
 	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);//开启相关中断
 
 	//Usart1 NVIC 配置
@@ -112,18 +95,12 @@ void uart_init(u32 bound){
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority =3;		//子优先级3
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
 	NVIC_Init(&NVIC_InitStructure);	//根据指定的参数初始化VIC寄存器、
-
-#endif
-	
 }
 
 
 void USART1_IRQHandler(void)                	//串口1中断服务程序
 {
 	u8 Res;
-#if SYSTEM_SUPPORT_OS 		//如果SYSTEM_SUPPORT_OS为真，则需要支持OS.
-	OSIntEnter();    
-#endif
 	if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)  //接收中断(接收到的数据必须是0x0d 0x0a结尾)
 	{
 		Res =USART_ReceiveData(USART1);//(USART1->DR);	//读取接收到的数据
@@ -136,22 +113,18 @@ void USART1_IRQHandler(void)                	//串口1中断服务程序
 				else USART_RX_STA|=0x8000;	//接收完成了 
 			}
 			else //还没收到0X0D
-			{	
+			{
 				if(Res==0x0d)USART_RX_STA|=0x4000;
 				else
 				{
 					USART_RX_BUF[USART_RX_STA&0X3FFF]=Res ;
 					USART_RX_STA++;
 					if(USART_RX_STA>(USART_REC_LEN-1))USART_RX_STA=0;//接收数据错误,重新开始接收	  
-				}		 
+				}
 			}
-		}   		 
-  } 
-#if SYSTEM_SUPPORT_OS 	//如果SYSTEM_SUPPORT_OS为真，则需要支持OS.
-	OSIntExit();  											 
-#endif
-} 
-#endif	
+		}
+  }
+}
 
  
 
--- software/USER/main.c
+++ software/USER/main.c
@@ -1,20 +1,30 @@
+/******************************************************************************
+* @ File name --> main.c
+* @ Author    --> By@ 鲁亮
+* @ Version   --> V1.0
+* @ Date      --> 07 - 16 - 2019
+* @ Brief     --> 系统主函数文件
+*
+* @ Copyright (C) 2019
+* @ All rights reserved	www.cqutlab.club
+*******************************************************************************
+*
+*                                  File Update
+* @ Version   --> V1.0
+* @ Author    --> By@鲁亮
+* @ Date      --> 07 - 16 - 2019
+* @ Revise    --> 新建文件
+*
+******************************************************************************/
+
 #include "sys.h"
 #include "delay.h"
 #include "usart.h"
 #include "led.h"
 #include "lcd.h"
 
-//ALIENTEK 探索者STM32F407开发板 实验13
-//LCD显示实验-库函数版本
-//技术支持：www.openedv.com
-//淘宝店铺：http://eboard.taobao.com  
-//广州市星翼电子科技有限公司  
-//作者：正点原子 @ALIENTEK
-
 int main(void)
 {
- 	u8 x=0;
-	u8 lcd_id[12];				//存放LCD ID字符串
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
 	delay_init(168);      //初始化延时函数
 	uart_init(115200);		//初始化串口波特率为115200
@@ -22,32 +32,13 @@ int main(void)
 	LED_Init();					  //初始化LED
  	LCD_Init();           //初始化LCD FSMC接口
 	POINT_COLOR=RED;      //画笔颜色：红色
-	sprintf((char*)lcd_id,"LCD ID:%04X",lcddev.id);//将LCD ID打印到lcd_id数组。				 	
-  while(1) 
+	POINT_COLOR=RED;	  
+	LCD_ShowString(30,40,210,24,24,(u8 *)"Smart Lab");	
+	LCD_ShowString(30,70,200,16,16,(u8 *)"407Lab");
+	LCD_ShowString(30,90,200,16,16,(u8 *)"www.cqutlab.club");    					 
+	LCD_ShowString(30,110,200,12,12,(u8 *)"2019/07/16");
+  while(1)
 	{
-		switch(x)
-		{
-			case 0:LCD_Clear(WHITE);break;
-			case 1:LCD_Clear(BLACK);break;
-			case 2:LCD_Clear(BLUE);break;
-			case 3:LCD_Clear(RED);break;
-			case 4:LCD_Clear(MAGENTA);break;
-			case 5:LCD_Clear(GREEN);break;
-			case 6:LCD_Clear(CYAN);break; 
-			case 7:LCD_Clear(YELLOW);break;
-			case 8:LCD_Clear(BRRED);break;
-			case 9:LCD_Clear(GRAY);break;
-			case 10:LCD_Clear(LGRAY);break;
-			case 11:LCD_Clear(BROWN);break;
-		}
-		POINT_COLOR=RED;	  
-		LCD_ShowString(30,40,210,24,24,"Explorer STM32F4");	
-		LCD_ShowString(30,70,200,16,16,"TFTLCD TEST");
-		LCD_ShowString(30,90,200,16,16,"ATOM@ALIENTEK");
- 		LCD_ShowString(30,110,200,16,16,lcd_id);		//显示LCD ID	      					 
-		LCD_ShowString(30,130,200,12,12,"2014/5/4");	      					 
-	  x++;
-		if(x==12)x=0;
 		LED0=!LED0;	 
 		delay_ms(1000);	
 	} 
